71ba6fd24c5ee8c5004c199b7441eb6b8cb88801
Ellisvelandia <eyis619l@gmail.com>
2023-02-06 20:12:56 -0500

    character:id-info

--- src/pages/CharacterInfo.jsx
+++ src/pages/CharacterInfo.jsx
@@ -16,23 +16,44 @@ const characterInfo = () => {
 
     getInfo();
   }, [id]);
-  const { name, images, about, info } = characters;
+  const { name, images, about, info, page } = characters;
 
   return (
-    <div className="relative w-full flex flex-col justify-center h-screen">
-      <h1 className="h-20 text-3xl">{name}</h1>
-      {images && images.length > 0 && <img src={images[0]} alt={name}  className="relative h-full w-full"/>}
-      <span className="absolute top-50">{about}</span>
-      <p className="text-red-900 text-5xl">{characters.Peso}</p>
-      {info && (
-        <ul className="">
-          {Object.entries(info).map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="h-auto w-full flex flex-col justify-center my-10">
+      <h1 className="text-3xl text-center my-4">{name}</h1>
+      <div className="flex justify-center">
+        {images && images.length > 0 && (
+          <img
+            src={images[0]}
+            alt={name}
+            className="object-fill rounded-full w-[300px] h-[300px]"
+          />
+        )}
+      </div>
+      <div className="flex flex-col w-full justify-center px-10 my-2">
+        <div className="md:px-4 px-1">
+          <span className="sm:text-justify text-center">{about}</span>
+        </div>
+        <div className="flex">
+          {info && (
+            <ul className="px-4 h-full flex flex-wrap">
+              {Object.entries(info).map(([key, value]) => (
+                <li key={key} className="flex my-4 p-4">
+                  <strong>{key}: </strong>
+                  {value}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className="w-full flex justify-center">
+        <a href={page} target="_blank">
+          <button className="flex justify-center w-auto h-auto p-2 border-red-900 border">
+            Leer mas
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
